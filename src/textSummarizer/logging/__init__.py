7d983ed07e48ed2704d9d08a44c6082c9d5d9107
ShawneilRodrigues <shawneilrodrigues@gmail.com>
2024-08-17 16:26:53 +0530

    tested logg

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -17,4 +17,4 @@ logging.basicConfig(
             logging.StreamHandler(sys.stdout)
        ]
        )
-logger=logging.getLogger("textSummarizerLogger")
+logger=logging.getLogger("textsummarizerLogger")
